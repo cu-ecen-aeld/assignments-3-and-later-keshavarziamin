4ef584aa550f8bdad8c5ff5b3fbab416208f246f
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-01-06 23:25:44 +0330

    modify manual-linux.sh

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -59,9 +59,9 @@ then
 fi
 
 # TODO: Create necessary base directories
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp var usr
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log 
+mkdir -p ${OUTDIR}/rootfs/{bin dev etc home lib lib64 proc sbin sys tmp var usr}
+mkdir -p ${OUTDIR}/rootfs/usr/{bin lib sbin}
+mkdir -p ${OUTDIR}/var/log 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -71,28 +71,45 @@ git clone git://busybox.net/busybox.git
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
     make distclean
-    make defconfig
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+config_prefix=${OUTDIR}/rootfs
 make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+make CONFIG_PREFIX=${config_prefix} ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
-${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
+${CROSS_COMPILE}readelf -a ${config_prefix}/bin/busybox | grep "program interpreter"
+${CROSS_COMPILE}readelf -a ${config_prefix}/bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
+cp -a ${SYSROOT}/lib/*      rootfs/lib
+cp -a ${SYSROOT}/lib64/*    rootfs/lib64
+
+
 
 # TODO: Make device nodes
+sudo mknod -m 666 ${config_prefix}/dev/null c 1 3 #The null device should be readable and writable by everyone
+sudo mknod -m 600 ${config_prefix}/dev/console c 5 1 #The console only needs to be accessible to root
 
 # TODO: Clean and build the writer utility
 
+
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/finder-app/finder-test.sh ${config_prefix}/home
+cp ${FINDER_APP_DIR}/finder-app/finder.sh ${config_prefix}/home
+cp ${FINDER_APP_DIR}/finder-app/autorun-qemu.sh ${config_prefix}/home
+cp -r ${FINDER_APP_DIR}/finer-app/conf ${config_prefix}/home
 
 # TODO: Chown the root directory
-
+cd rootfs/
+sudo chown -R root:root *
 # TODO: Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f initramfs.cpio
