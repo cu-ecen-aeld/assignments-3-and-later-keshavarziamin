f2ea16a5eae380ad7923f6a56bb2cd66192aac29
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-03-09 08:38:30 +0330

    modify server

--- server/testlog.c
+++ server/testlog.c
@@ -1,12 +1,14 @@
 #include <syslog.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main()
 {
     setlogmask(LOG_UPTO(LOG_NOTICE));
-    openlog("exampleprog", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    openlog("exampleprog ", LOG_CONS | LOG_PID | LOG_NDELAY, LOG_LOCAL1);
+    printf("Program started by User %d\r\n", getuid());
     syslog(LOG_NOTICE, "Program started by User %d", getuid());
-    syslog(LOG_INFO, "A tree falls in a forest");
+    syslog(LOG_NOTICE, "A tree falls in a forest");
     closelog();
 }
