7df40774539c82ae342710f6f87d0f0266e8c701
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-03-12 10:03:16 +0330

    test socket success

--- server/aesdsocket.c
+++ server/aesdsocket.c
@@ -12,15 +12,17 @@
 #include <signal.h>
 
 #define SOCK_DATA_FILE "/var/tmp/aesdsocketdata"
-#define LENGTH_OF_BUFFER 512
+#define LENGTH_OF_BUFFER 2024
 #define RETERR -1
 
 #define __EXIT(msg_)                    \
     {                                   \
         perror("ERROR:" msg_ ">>");     \
         syslog(LOG_ERR, "ERROR:" msg_); \
+        unlink(SOCK_DATA_FILE);         \
         exit(RETERR);                   \
     }
+#define __SUCCESS() printf("%s succeeded.\r\n", __func__)
 
 bool acceptedExit = false;
 int enable = 1;
@@ -32,14 +34,15 @@ struct net_t
 };
 
 void signal_create(void);
+void daemon_create(char *argv);
 void server_createSocket(struct net_t *server);
-void file_create(FILE *file);
+void file_create(FILE **file);
 void file_remove(FILE *file);
 void server_connectSocket(struct net_t *server, const uint16_t port);
-void server_waitToAcceptCient(struct net_t *server, struct net_t *client);
+void server_waitToAcceptClient(struct net_t *server, struct net_t *client);
 
 void signal_acceptExit(int signalType);
-void server_receiveData(FILE *file, int *serverfd);
+void server_echoData(FILE **file, int cleinrfd, uint8_t *rxbuff);
 void server_exit(FILE *file, struct net_t *sever);
 void printUsage(void);
 
@@ -48,23 +51,38 @@ int main(int argc, char *argv[])
     struct net_t server, client;
     in_port_t port = 9000;
     openlog(NULL, 0, LOG_USER); // open system logger
-    // uint8_t rxbuff[LENGTH_OF_BUFFER + 1] = {0};
+    uint8_t rxbuff[LENGTH_OF_BUFFER + 1] = {0};
 
+    // create and open file in order to write recived data
+    FILE *file = {0};
+    file_create(&file);
+
+    // create signal
     signal_create();
 
-    // create a server
+    // create a connect to socket (localhost)
     server_createSocket(&server);
     server_connectSocket(&server, port);
-    server_waitToAcceptCient(&server,&client);
 
-    // FILE *file;
-    // // file_create(file);
-    // int num_read = 0;
-    // while (1)
-    // {
-    //     num_read = read(client->addr, )
-    // }
+    // create a daemon if argv == -d
+    if (argc == 2)
+        daemon_create(argv[1]);
 
+    while (1)
+    {
+        // accept client before receiving data
+        server_waitToAcceptClient(&server, &client);
+        /*
+         * receive and echo data from and to client.
+         * data must to be write in file after receiving,
+         * then read them before transmmiting.
+         */
+        server_echoData(&file, client.fd, rxbuff);
+    }
+
+    unlink(SOCK_DATA_FILE);
+    int err = fclose(file);
+    perror("closing file: ");
     return 0;
 }
 
@@ -82,6 +100,23 @@ void signal_create(void)
     err = sigaction(SIGINT, &action, NULL);
     if (err)
         __EXIT("creating signal(SIGINT) failed.")
+
+    __SUCCESS();
+}
+
+void daemon_create(char *argv)
+{
+    if (argv == NULL)
+        return;
+
+    pid_t pid = 0;
+    if (strcmp(argv, "-d"))
+        pid = daemon(0, 0);
+
+    if (pid == -1)
+        __EXIT("forking the process failed");
+
+    __SUCCESS();
 }
 
 void server_createSocket(struct net_t *server)
@@ -89,13 +124,14 @@ void server_createSocket(struct net_t *server)
     if (!server)
         __EXIT("The pinter of server is null.");
 
-    server->fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    server->fd = socket(AF_INET, SOCK_STREAM, 0);
     if (server->fd == -1)
         __EXIT("creating socket failed.")
 
     int err = setsockopt(server->fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
     if (err)
-        __EXIT("Setting option on socket failed.")
+        __EXIT("Setting option on socket failed.");
+    __SUCCESS();
 }
 
 void server_connectSocket(struct net_t *server, const uint16_t port)
@@ -104,7 +140,7 @@ void server_connectSocket(struct net_t *server, const uint16_t port)
     memset(&server->addr, 0, sizeof(struct sockaddr_in)); // clear buffer of struct
     server->addr.sin_family = AF_INET;
     server->addr.sin_addr.s_addr = INADDR_ANY;
-    server->addr.sin_port = port;
+    server->addr.sin_port = htons(port);
 
     int err = bind(server->fd, (struct sockaddr *)&server->addr, sizeof(struct sockaddr));
     if (err < 0)
@@ -113,9 +149,11 @@ void server_connectSocket(struct net_t *server, const uint16_t port)
     err = listen(server->fd, 10);
     if (err)
         __EXIT("listening socket failed.");
+
+    __SUCCESS();
 }
 
-void server_waitToAcceptCient(struct net_t *server, struct net_t *client)
+void server_waitToAcceptClient(struct net_t *server, struct net_t *client)
 {
     char clientIp[INET_ADDRSTRLEN] = {0};
     socklen_t socklen = sizeof(struct sockaddr);
@@ -123,29 +161,55 @@ void server_waitToAcceptCient(struct net_t *server, struct net_t *client)
 
     printf("server is watting to connect to client >> \r\n");
 
-    while (1)
-    {
-        client->fd = accept(server->fd, (struct sockaddr *)&client->addr, &socklen);
-        if (client->fd == -1)
-            __EXIT("accepting socket failed.");
-    }
+    client->fd = accept(server->fd, (struct sockaddr *)&client->addr, &socklen);
+    if (client->fd == -1)
+        __EXIT("accepting socket failed.");
 
     inet_ntop(AF_INET, &client->addr.sin_addr, clientIp, INET_ADDRSTRLEN);
     printf("Accepted connection from %s\r\n", clientIp);
     syslog(LOG_DEBUG, "Accepted connection from %s", clientIp);
+
+    __SUCCESS();
 }
 
-void server_receiveData(FILE *file, int *serverfd)
+void server_echoData(FILE **file, int clientfd, uint8_t *rxbuff)
 {
+    int len = 0;
+    uint8_t txBuff[LENGTH_OF_BUFFER + 1] = {0};
+    while (1)
+    {
+        // read data from buffer of client
+        len = read(clientfd, rxbuff, LENGTH_OF_BUFFER);
+        if (len < 0)
+            __EXIT("Reading data from the client failed.");
+        if (len == 0)
+            break;
 
-    int buffer[LENGTH_OF_BUFFER];
+        // write data on file
+        rxbuff[len] = '\0';
+        len = fprintf(*file, "%s", (char *)rxbuff);
+        if (len == -1)
+            __EXIT("writing data on file failed.");
 
-    if (recv(*serverfd, buffer, LENGTH_OF_BUFFER, 0) < 0)
-        __EXIT("receiving from client failed.");
+        // return the start of STREAM if there is '\n'
+        if (rxbuff[len - 1] == '\n')
+            rewind(*file);
+            
+        // read data written from file
+        while (1)
+        {
+            len = fread(txBuff, 1, LENGTH_OF_BUFFER, *file);
+            if (len == 0)
+                break;
+            if (len == -1)
+                __EXIT("Reading from file failed.");
 
-    size_t retSize = fwrite(buffer, sizeof(int), LENGTH_OF_BUFFER, file);
-    if (retSize < 0)
-        __EXIT("writing in file failed.");
+            len = write(clientfd, txBuff, len);
+            if (len == -1)
+                __EXIT("Echoing data to client failed."); /* code */
+        }
+    }
+    __SUCCESS();
 }
 
 void signal_acceptExit(int signalType)
@@ -173,10 +237,10 @@ void server_exit(FILE *file, struct net_t *server)
     exit(EXIT_SUCCESS);
 }
 
-void file_create(FILE *file)
+void file_create(FILE **file)
 {
-    file = fopen(SOCK_DATA_FILE, "a+");
-    if (!file)
+    *file = fopen(SOCK_DATA_FILE, "a+");
+    if (!*file)
         __EXIT("creating file failed.")
 }
 
