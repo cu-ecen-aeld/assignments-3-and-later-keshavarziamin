95b3f0f357f3f9faaf065b102134b020fdfbeaaa
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-03-13 10:43:57 +0330

    modify usename directory

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,7 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+username=$(cat /etc/finder-app/conf/username.txt)
 RESULT_ADDR="/tmp/assignment4-result.txt"
 
 if [ $# -lt 2 ]; then
