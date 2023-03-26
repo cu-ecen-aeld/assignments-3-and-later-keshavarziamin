2376279754cca8988e2cacb71789ffcc39a38f2c
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-03-26 16:35:38 +0330

    aesdsocket-start-stop.sh

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,11 +3,11 @@
 case "$1" in
 start)
     echo "starting aesdsocket"
-    start-stop-daemon -S -n aesdsocket -d
+    start-stop-daemon -S -n aesdsocket -d usr/bin/aesdsocket
     ;;
 stop)
     echo "stoping aesdsocket"
-    start-stop-daemon -K -n aesdsocket usr/bin/aesdsocket
+    start-stop-daemon -K -n aesdsocket 
     ;;
 *)
     echo "Usage: $0 {start|stop}"
