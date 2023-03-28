afdeb2c34687a106b55e66e4cab3e57d06cb3aee
keshavarzi.amin <keshavarzi.amin@gmail.com>
2023-03-28 15:20:51 +0330

    change aesdsocket* type

---
+++ server/aesdsocket-start-stop.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+case "$1" in
+start)
+    echo "starting aesdsocket"
+    start-stop-daemon -S -n aesdsocket usr/bin/aesdsocket -- -d
+    ;;
+stop)
+    echo "stoping aesdsocket"
+    start-stop-daemon -K -n aesdsocket 
+    ;;
+*)
+    echo "Usage: $0 {start|stop}"
+    exit 1
+    ;;
+esac
+exit 0
